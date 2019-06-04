f7751141c06a005f65b5941da0857281457eb2d2
Greg Goltsov <gregory@goltsov.info>
2019-06-04 20:26:45 +0100

    Updates

--- project.clj
+++ project.clj
@@ -12,9 +12,10 @@
 
   :source-paths ["src"]
 
-  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+  :aliases {"fig"        ["trampoline" "run" "-m" "figwheel.main"]
              "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
              "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+             "fig:live"  ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "live"]
              "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" clojurebridge-landing-page.test-runner]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
